56954c71b76c702ab506b3f99bf628216824a801
kjubik <wojt.kubicki@gmail.com>
2024-08-18 22:13:53 +0200

    Uncomment fetching data from Directus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
-<!-- <script>
+<script>
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
 <h1>{data.global.title}</h1>
-<p>{data.global.description}</p> -->
-
-
-Hello
+<p>{data.global.description}</p>
